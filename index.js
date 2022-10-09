67dbd797dab8335eff193420f8cf3ff6750bad6c
yasushi0833 <yasushi0833@gmail.com>
2022-10-10 01:32:25 +0900

    修正

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "public")));
 //   });
 
 app.post("/api/v1/quiz", (req, res) => {
-    const answer = req.body.answer;A
+    const answer = req.body.answer;
     if (answer === "2"){
         // res.send("<h1>正解</h1>");
         res.redirect("/correct.html");
